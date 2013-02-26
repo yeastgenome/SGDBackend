d7d4c6526ce261605d5894956655d5fcdc5077be
kpaskov <kpaskov@stanford.edu>
2013-02-25 18:42:58 -0800

    Cytoscape graph is clickable.

--- jsonify/graph.py
+++ jsonify/graph.py
@@ -3,6 +3,8 @@ Created on Feb 19, 2013
 
 @author: kpaskov
 '''
+from jsonify.mini import bioent_mini
+from jsonify.small import biorel_small
 from model_new_schema.biorelation import Biorelation
 from sgd2.models import DBSession
 
@@ -11,8 +13,8 @@ def create_graph(bioent):
     edges = []
     
     bioent_id = str(bioent.id)
-    bioent_name = str(bioent.secondary_name)
-    nodes.append({'id':bioent_id, 'label':bioent_name, 'focus':'1'})
+    bioent_json = bioent_mini(bioent)
+    nodes.append({'id':str(bioent_id), 'label':str(bioent_json['name']), 'focus':'1', 'link':str(bioent_json['link'])})
     
     neighbor_ids = set()
     neighbor_ids.add(bioent.id)
@@ -25,7 +27,8 @@ def create_graph(bioent):
                 neighbor = interaction.source_bioent
             
             if not neighbor.id in neighbor_ids:
-                nodes.append({'id':str(neighbor.id), 'label':str(neighbor.secondary_name), 'focus':'0'})
+                neighbor_json = bioent_mini(neighbor)
+                nodes.append({'id':str(neighbor.id), 'label':str(neighbor_json['name']), 'focus':'0', 'link':str(neighbor_json['link'])})
                 neighbor_ids.add(neighbor.id)
         
     interactions = set(DBSession.query(Biorelation).filter(Biorelation.source_bioent_id.in_(neighbor_ids)).all())
@@ -33,10 +36,10 @@ def create_graph(bioent):
 
     for interaction in interactions:
         if interaction.evidence_count > 2 and interaction.source_bioent_id in neighbor_ids and interaction.sink_bioent_id in neighbor_ids:
-            edges.append({ 'id': str(interaction.id), 'target': str(interaction.source_bioent_id), 'source': str(interaction.sink_bioent_id), 'label': str(interaction.name)})      
+            edges.append({ 'id': str(interaction.id), 'target': str(interaction.source_bioent_id), 'source': str(interaction.sink_bioent_id), 'label': str(interaction.name), 'link':'/biorel/' + str(interaction.name)})      
         
-    graph = {'dataSchema': {'nodes': [ { 'name': "label", 'type': "string" }, { 'name': "focus", 'type': "string" }],
-                            'edges': [ { 'name': "label", 'type': "string" }]},
+    graph = {'dataSchema': {'nodes': [ { 'name': "label", 'type': "string" }, { 'name': "focus", 'type': "string" }, {'name':'link', 'type':'string'}],
+                            'edges': [ { 'name': "label", 'type': "string" }, {'name':'link', 'type':'string'}]},
             'data': {'nodes': nodes, 'edges': edges}}
     return graph
     
