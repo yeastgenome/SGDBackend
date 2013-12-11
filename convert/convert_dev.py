5272bc9b9abd611183ee4553535bc21b738b30d3
kpaskov <kpaskov@stanford.edu>
2013-12-11 15:21:37 -0800

    Made small changes to the conversion scripts so that "observable" is transformed into YPO Ontology.

--- convert/convert_dev.py
+++ convert/convert_dev.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     #pastry_dev_converter.convert_chemical()
     #pastry_dev_converter.convert_chemical()
     #pastry_dev_converter.convert_phenotype()
-    #pastry_dev_converter.convert_bioconcept()
+    pastry_dev_converter.convert_bioconcept()
     #pastry_dev_converter.convert_bioconcept_in_depth()
     #pastry_dev_converter.convert_phenotype()
     #pastry_dev_converter.convert_evelements()
     #pastry_dev_converter.convert_reference()
-    pastry_dev_converter.convert_go()
+    #pastry_dev_converter.convert_go()
 
     #Dev_nex -> Dev_perf
     #dev_perf_converter = NexPerfConverter(config.NEX_DBTYPE, 'sgd-dev-db.stanford.edu:1521', config.NEX_DBNAME, config.NEX_SCHEMA, config.NEX_DBUSER, config.NEX_DBPASS, 
