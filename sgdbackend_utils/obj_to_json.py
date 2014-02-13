3c65b9fd27a16364f681033f5c1aa0afcdaa671c
kpaskov <kpaskov@stanford.edu>
2014-02-13 12:02:53 -0800

    Small fix - added locus format name to cached proteins.

--- sgdbackend_utils/obj_to_json.py
+++ sgdbackend_utils/obj_to_json.py
@@ -11,7 +11,7 @@ def locus_to_json(bioent):
 
 def protein_to_json(bioent, id_to_bioent):
     bioent_json = bioent_to_json(bioent)
-    bioent_json['locus'] = minimize_json(id_to_bioent[bioent.locus_id])
+    bioent_json['locus'] = minimize_json(id_to_bioent[bioent.locus_id], include_format_name=True)
     return bioent_json
 
 def bioent_to_json(bioent):
