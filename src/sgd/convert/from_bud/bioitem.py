cb1d1003ecacac469f7af9e31c21a305b77836d3
kpaskov <kpaskov@stanford.edu>
2014-11-16 20:06:19 -0800

    Loading giltae's strains.

--- src/sgd/convert/from_bud/bioitem.py
+++ src/sgd/convert/from_bud/bioitem.py
@@ -330,7 +330,7 @@ def make_contig_starter(bud_session_maker, nex_session_maker):
         for feature in bud_session.query(Feature).filter(or_(Feature.type == 'chromosome', Feature.type == 'plasmid')).all():
             for sequence in feature.sequences:
                 if sequence.is_current == 'Y':
-                    contig_key = Contig.create_contig_name('Chromosome ' + feature.name)
+                    contig_key = Contig.create_contig_name('Chromosome ' + feature.name, None)
                     genbank_accession = None if contig_key not in s288c_chromosome_to_genbank_id else s288c_chromosome_to_genbank_id[contig_key]
                     yield {'display_name': 'Chromosome ' + feature.name,
                            'source': key_to_source['SGD'],
