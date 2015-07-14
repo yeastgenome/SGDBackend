9e95e5d4acb020548cb10f8eeb7374c8911d4e7e
Gail Binkley <gail.binkley@stanford.edu>
2015-07-14 14:49:59 -0700

    Add triggers for EVIDENCE tables

--- src/sgd/model/nex/triggers/load_nex_triggers.sql
+++ src/sgd/model/nex/triggers/load_nex_triggers.sql
@@ -10,14 +10,29 @@
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Dbuser_AUDR.trg
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Source_BIUR.trg
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Source_AUDR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Chemical_BIUR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Chemical_AUDR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalAlias_BIUR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalAlias_AUDR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalUrl_BIUR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalUrl_AUDR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalRelation_BIUR.trg
-@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalRelation_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Chemical_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Chemical_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalAlias_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalAlias_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalUrl_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalUrl_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalRelation_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/ChemicalRelation_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Enzyme_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Enzyme_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EnzymeAlias_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EnzymeAlias_AUDR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EnzymeUrl_BIUR.trg
+--@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EnzymeUrl_AUDR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Evidence_BIUR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Evidence_AUDR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceAlias_BIUR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceAlias_AUDR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceUrl_BIUR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceUrl_AUDR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceRelation_BIUR.trg
+@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/EvidenceRelation_AUDR.trg
+
 
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Taxonomy_BIUR.trg
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/Taxonomy_AUDR.trg
@@ -27,4 +42,5 @@
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/TaxonomyUrl_AUDR.trg
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/TaxonomyRelation_BIUR.trg
 --@/db0/home/oracle/SGDBackend/src/sgd/model/nex/triggers/TaxonomyRelation_AUDR.trg
+
 
