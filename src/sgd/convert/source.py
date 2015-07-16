bb167c4f30107c30fd1345c6c752f8a02b2f05b4
Shuai Weng <sweng@stanford.edu>
2015-07-16 14:44:07 -0700

    created/updated loading programs

--- src/sgd/convert/source.py
+++ src/sgd/convert/source.py
@@ -1,43 +1,49 @@
-from src.sgd.convert.into_curate import basic_convert
+from src.sgd.convert import basic_convert
+from src.sgd.convert.util import is_number
 
-__author__ = 'kpaskov'
-
-other_sources = ['SGD', 'GO', 'PROSITE', 'Gene3D', 'SUPERFAMILY', 'TIGRFAM', 'Pfam', 'PRINTS',
-                                        'PIRSF', 'JASPAR', 'SMART', 'PANTHER', 'ProDom', 'DOI',
-                                        'PubMedCentral', 'PubMed', '-', 'ECO', 'TMHMM', 'SignalP', 'PhosphoGRID',
-                                        'GenBank/EMBL/DDBJ', 'Phobius', 'SO']
-
-ok_codes = {('ALIAS', 'ALIAS_TYPE'), ('DBXREF', 'SOURCE'), ('EXPERIMENT', 'SOURCE'), ('FEATURE', 'SOURCE'),
-                ('GO_ANNOTATION', 'SOURCE'), ('HOMOLOG', 'SOURCE'), ('INTERACTION', 'SOURCE'), ('PHENOTYPE', 'SOURCE'),
-                ('REFTYPE', 'SOURCE'), ('REFERENCE', 'SOURCE'), ('URL', 'SOURCE')}
-
+__author__ = 'sweng66'
 
 def source_starter(bud_session_maker):
+
     from src.sgd.model.bud.cv import Code
     bud_session = bud_session_maker()
 
+    id2dateCreated = {}
+    id2createdBy = {}
     for bud_obj in bud_session.query(Code).all():
-        if (bud_obj.tab_name, bud_obj.col_name) in ok_codes and bud_obj.code_value != 'Publication':
-            obj_json = {'display_name': bud_obj.code_value,
-                        'bud_id': bud_obj.id,
-                        'date_created': str(bud_obj.date_created),
-                        'created_by': bud_obj.created_by}
+        id2dateCreated[bud_obj.id] = bud_obj.date_created
+        id2createdBy[bud_obj.id] = bud_obj.created_by
 
-            if bud_obj.description is not None:
-                obj_json['description'] = bud_obj.description
-
-            yield obj_json
-
-    for source in other_sources:
-        yield {'display_name': source}
-
-    bud_session.close()
-
-
-def convert(bud_db, nex_db):
-    basic_convert(bud_db, nex_db, source_starter, 'source', lambda x: x['display_name'])
-
-
+    file_names = ['src/sgd/convert/data/source_data_toload.txt']
+    
+    for file_name in file_names:
+        print file_name
+        f = open(file_name, 'rU')
+        header = True
+        for line in f:
+            if header:
+                header = False
+            else:
+                items = line.split('\t')
+                source = items[0]
+                bud_id = items[1]
+                desc = items[2].replace('"', '')
+                obj_json = {'display_name': source }
+                if is_number(bud_id):
+                    id = int(bud_id)
+                    obj_json['bud_id'] = id
+                    if id2dateCreated.get(id):
+                        obj_json['date_created'] = str(id2dateCreated.get(id))
+                    if id2createdBy.get(int(bud_id)):
+                        obj_json['created_by'] = id2createdBy.get(id)
+                if desc:
+                    obj_json['description'] = desc
+                
+                yield obj_json
+                    
 if __name__ == '__main__':
-    convert('pastry.stanford.edu:1521', 'curator-dev-db')
+    from src.sgd.convert import config
+    basic_convert(config.BUD_HOST, config.NEX_HOST, source_starter, 'source', lambda x: x['display_name'])
+
+
 
--- src/sgd/convert/taxonomy.py
+++ src/sgd/convert/taxonomy.py
@@ -1,31 +1,42 @@
-from src.sgd.convert.into_curate import basic_convert, remove_nones
+from src.sgd.convert import basic_convert, remove_nones
 from sqlalchemy.orm import joinedload
 
 
 __author__ = 'kpaskov'
+## updated by sweng66
 
 
-def load_aliases(bud_obj, bud_session):
+def load_aliases(bud_obj, bud_session, secondary_common_name):
     from src.sgd.model.bud.taxonomy import TaxonomyAlias
 
     aliases = []
-    for bud_obj in bud_session.query(TaxonomyAlias).filter_by(id=bud_obj.id).all():
+    for alias_obj in bud_session.query(TaxonomyAlias).filter_by(taxon_id=bud_obj.id).all():
         aliases.append(remove_nones({
-            "display_name": bud_obj.synonym,
-            "alias_type": 'None',
-            'source': {'display_name': '-'}
+            "display_name": alias_obj.synonym,
+            "alias_type": 'Synonym',
+            "source": {'display_name': 'NCBI'},
+            "bud_id": int(alias_obj.id)
         }))
+
+    for common_name in secondary_common_name:
+        aliases.append({
+            "display_name": common_name,
+            "alias_type": 'Secondary common name',
+            "source": {'display_name': 'NCBI'}
+        })
+
     return aliases
+
 
 def load_relations(bud_obj, bud_session):
     from src.sgd.model.bud.taxonomy import TaxonomyRelation
 
     relations = []
-    for bud_obj in bud_session.query(TaxonomyRelation).filter_by(parent_id=bud_obj.id).filter_by(generation=1).all():
+    for relation_obj in bud_session.query(TaxonomyRelation).filter_by(parent_id=bud_obj.id).filter_by(generation=1).all():
         relations.append(remove_nones({
-            "display_name": bud_obj.child.name,
-            'source': {'display_name': '-'},
-            "relation_type": 'None'
+            "display_name": relation_obj.child.name,
+            'source': {'display_name': 'NCBI'},
+            "relation_type": 'is a'
         }))
     return relations
 
@@ -34,32 +45,44 @@ def taxonomy_starter(bud_session_maker):
     from src.sgd.model.bud.taxonomy import TaxonomyRelation
     bud_session = bud_session_maker()
 
+    # only load taxonomy for family "Saccharomycetaceae" (taxon_id=4893) and lower
     for relation in bud_session.query(TaxonomyRelation).options(joinedload(TaxonomyRelation.child)).filter_by(parent_id=4893).all():
         bud_obj = relation.child
-        obj_json = remove_nones({'display_name': bud_obj.name,
-                                 'bud_id': bud_obj.id,
-                                 'common_name': bud_obj.common_name,
-                                 'rank': bud_obj.rank,
-                                 'source': {'display_name': '-'},
-                                 'date_created': str(bud_obj.date_created),
-                                 'created_by': bud_obj.created_by})
+    
+        obj_json = {'display_name': bud_obj.name,                  
+                    'taxid': int(bud_obj.id),
+                    'rank': bud_obj.rank,    
+                    'source': {'display_name': 'NCBI'},
+                    'date_created': str(bud_obj.date_created),                  
+                    'created_by': bud_obj.created_by}     
+
+        common_name_str = bud_obj.common_name
+        
+        secondary_common_name = []
+        if common_name_str != None:
+            common_name_list = common_name_str.split("|") 
+            name = common_name_list.pop(0)
+            obj_json['common_name'] = name
+            secondary_common_name = common_name_list
 
         #Load aliases
-        obj_json['aliases'] = load_aliases(bud_obj, bud_session)
-
+        obj_json['aliases'] = load_aliases(bud_obj, bud_session, secondary_common_name)
+        
         #Load children
         obj_json['children'] = load_relations(bud_obj, bud_session)
 
-        print bud_obj.name
+        obj_json['urls'] = [{"display_name": 'NCBI Taxonomy',
+                             "url_type": 'NCBI Taxonomy',
+                             "source": {'display_name': 'NCBI'},
+                             "link": "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+str(bud_obj.id)}]
+
         yield obj_json
 
     bud_session.close()
 
-
-def convert(bud_db, nex_db):
-    basic_convert(bud_db, nex_db, taxonomy_starter, 'taxonomy', lambda x: x['display_name'])
-
-
 if __name__ == '__main__':
-    convert('pastry.stanford.edu:1521', 'curator-dev-db')
+    from src.sgd.convert import config
+    basic_convert(config.BUD_HOST, config.NEX_HOST, taxonomy_starter, 'taxonomy', lambda x: x['display_name'])
+
+
 
