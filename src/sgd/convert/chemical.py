15eb699fdc56cae9437e15d19bdb5a2c4c7a970c
Shuai Weng <sweng@stanford.edu>
2015-07-18 07:31:14 -0700

    fixed to work with new schema

--- src/sgd/convert/chemical.py
+++ src/sgd/convert/chemical.py
@@ -1,37 +1,15 @@
-from src.sgd.convert.into_curate import basic_convert, remove_nones
+from src.sgd.convert import basic_convert, remove_nones
 from sqlalchemy.sql.expression import or_
 
 __author__ = 'kpaskov'
 
-key_switch = {'id': 'chebi_id', 'name': 'display_name', 'def': 'description', 'created_by': 'created'}
+key_switch = {'id': 'chebi_id', 'name': 'display_name', 'def': 'description'}
 
 def chemical_starter(bud_session_maker):
-    from src.sgd.model.bud.cv import CVTerm
-    from src.sgd.model.bud.phenotype import ExperimentProperty
-
-    bud_session = bud_session_maker()
-
-    chebi_to_date_created = {}
-    for bud_obj in bud_session.query(CVTerm).filter(CVTerm.cv_no == 3).all():
-        if bud_obj.dbxref_id is not None:
-            chebi_to_date_created[bud_obj.dbxref_id] = (str(bud_obj.date_created), bud_obj.created_by)
-        else:
-            yield remove_nones({'display_name': bud_obj.name,
-                                'source': {'display_name': 'SGD'},
-                                'bud_id': bud_obj.id,
-                                'description': bud_obj.definition,
-                                'date_created': str(bud_obj.date_created),
-                                'created_by': bud_obj.created_by})
-
-    for bud_obj in bud_session.query(ExperimentProperty).filter(or_(ExperimentProperty.type=='Chemical_pending', ExperimentProperty.type == 'chebi_ontology')).all():
-        yield {'display_name': bud_obj.value,
-               'bud_id': bud_obj.id,
-               'source': {'display_name': 'SGD'},
-               'date_created': str(bud_obj.date_created),
-               'created_by': bud_obj.created_by}
-
+    
     terms = []
     parent_to_children = dict()
+    # downloaded from ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.obo
     f = open('src/sgd/convert/data/chebi.obo', 'r')
     term = None
     for line in f:
@@ -49,15 +27,22 @@ def chemical_starter(bud_session_maker):
                     quotation_split = pieces[1].split('"')
                     display_name = quotation_split[1]
                     alias_type = quotation_split[2].split('[')[0].strip()[:40]
+                    ### 
+                    if alias_type not in ('EXACT', 'RELATED'):
+                        continue
                     if len(display_name) < 500 and (display_name, alias_type) not in [(x['display_name'], x['alias_type']) for x in term['aliases']]:
                         term['aliases'].append({'display_name': display_name, "alias_type": alias_type, "source": {"display_name": "EBI"}})
                 elif pieces[0] == 'is_a':
                     parent = pieces[1].split('!')[0].strip()
                     if parent not in parent_to_children:
                         parent_to_children[parent] = []
-                    parent_to_children[parent].append({'chebi_id': term['chebi_id'], 'display_name': term['display_name'], 'source': {'display_name': 'EBI'}, 'relation_type': 'is_a'})
+                    parent_to_children[parent].append({'chebi_id': term['chebi_id'], 'display_name': term['display_name'], 'source': {'display_name': 'EBI'}, 'relation_type': 'is a'})
                 elif pieces[0] in key_switch:
-                    term[key_switch[pieces[0]]] = pieces[1]
+                    text = pieces[1]
+                    quotation_split = pieces[1].split('"')
+                    if pieces[0] == 'def':
+                        text = quotation_split[1]
+                    term[key_switch[pieces[0]]] = text
                 else:
                     term[pieces[0]] = pieces[1]
     f.close()
@@ -66,21 +51,16 @@ def chemical_starter(bud_session_maker):
         chebi_id = term['chebi_id']
         print chebi_id
         term['children'] = [] if chebi_id not in parent_to_children else parent_to_children[chebi_id]
-        term['urls'].append({'display_name': 'EBI',
+        term['urls'].append({'display_name': 'ChEBI',
                               'link': 'http://www.ebi.ac.uk/chebi/searchId.do?chebiId=' + term['chebi_id'],
                               'source': {'display_name': 'EBI'},
-                              'url_type': 'External'})
-
-        if chebi_id in chebi_to_date_created:
-            term['date_created'], term['created_by'] = chebi_to_date_created[chebi_id]
+                              'url_type': 'ChEBI'})
         yield term
 
     bud_session.close()
 
-def convert(bud_db, nex_db):
-    basic_convert(bud_db, nex_db, chemical_starter, 'chemical', lambda x: x['display_name'])
-
-
 if __name__ == '__main__':
-    convert('pastry.stanford.edu:1521', 'curator-dev-db')
+    from src.sgd.convert import config
+    basic_convert(config.BUD_HOST, config.NEX_HOST, chemical_starter, 'chemical', lambda x: x['display_name'])
+
 
