b2460f7719ef708924fce7de086c6b4e974e1ced
Shuai Weng <sweng@stanford.edu>
2015-07-22 15:14:56 -0700

    fixed a minor thing

--- src/sgd/convert/sequencefeature.py
+++ src/sgd/convert/sequencefeature.py
@@ -1,12 +1,14 @@
-from src.sgd.convert.into_curate import basic_convert
+from src.sgd.convert import basic_convert
 
 __author__ = 'kpaskov'
+## updated by sweng66
 
-key_switch = {'id': 'so_id', 'name': 'display_name', 'def': 'description', 'created_by': 'created'}
+key_switch = {'id': 'so_id', 'name': 'display_name', 'def': 'description'}
 
 def sequencefeature_starter(bud_session_maker):
     terms = []
     parent_to_children = dict()
+    ## check it out from  https://github.com/The-Sequence-Ontology/SO-Ontologies/
     f = open('src/sgd/convert/data/so-xp-simple.obo', 'r')
     term = None
     for line in f:
@@ -22,36 +24,43 @@ def sequencefeature_starter(bud_session_maker):
             if len(pieces) == 2:
                 if pieces[0] == 'synonym':
                     quotation_split = pieces[1].split('"')
-                    term['aliases'].append({'display_name': quotation_split[1], "alias_type": quotation_split[2].split('[')[0].strip(), "source": {"display_name": "SO"}})
+                    type = quotation_split[2].split('[')[0].strip()
+                    alias_type = type.split(' ')[0]
+                    term['aliases'].append({'display_name': quotation_split[1], "alias_type": alias_type, "source": {"display_name": "SO"}})
                 elif pieces[0] == 'is_a':
                     parent = pieces[1].split('!')[0].strip()
                     if parent not in parent_to_children:
                         parent_to_children[parent] = []
-                    parent_to_children[parent].append({'so_id': term['so_id'], 'display_name': term['display_name'], 'source': {'display_name': 'SO'}, 'relation_type': 'is_a'})
+                    parent_to_children[parent].append({'so_id': term['so_id'], 'display_name': term['display_name'], 'source': {'display_name': 'SO'}, 'relation_type': 'is a'})
                 elif pieces[0] in key_switch:
-                    term[key_switch[pieces[0]]] = pieces[1]
-                else:
-                    term[pieces[0]] = pieces[1]
+                    text = pieces[1]
+                    quotation_split = pieces[1].split('"')
+                    if pieces[0] == 'def':
+                        text = quotation_split[1]
+                    term[key_switch[pieces[0]]] = text
+                # else:
+                #    term[pieces[0]] = pieces[1]
     f.close()
 
     for term in terms:
         so_id = term['so_id']
         print so_id
         term['children'] = [] if so_id not in parent_to_children else parent_to_children[so_id]
-        term['urls'].append({'display_name': 'Sequence Ontology',
+        term['urls'].append({'display_name': 'MISO',
                               'link': 'http://www.sequenceontology.org/miso/current_svn/term/' + term['so_id'],
-                              'source': {'display_name': 'SO'},
-                              'url_type': 'External'})
+                              'source': {'display_name': 'OBO Foundry'},
+                              'url_type': 'MISO'})
         term['urls'].append({'display_name': 'OLS',
                               'link': 'https://www.ebi.ac.uk/ontology-lookup/?termId=' + term['so_id'],
                               'source': {'display_name': 'EBI'},
-                              'url_type': 'External'})
+                              'url_type': 'OLS'})
         yield term
-
-def convert(bud_db, nex_db):
-    basic_convert(bud_db, nex_db, sequencefeature_starter, 'sequencefeature', lambda x: x['display_name'])
 
 
 if __name__ == '__main__':
-    convert('pastry.stanford.edu:1521', 'curator-dev-db')
+    from src.sgd.convert import config
+    basic_convert(config.BUD_HOST, config.NEX_HOST, sequencefeature_starter, 'sequencefeature', lambda x: x['display_name'])
+
+
+
 
