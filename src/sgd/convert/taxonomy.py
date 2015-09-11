32249547835c6a5cf826f1d917b6ab96b7882490
Shuai Weng <sweng@stanford.edu>
2015-09-11 11:56:25 -0700

    load taxonomy data from obo file instead of from BUD

--- src/sgd/convert/taxonomy.py
+++ src/sgd/convert/taxonomy.py
@@ -1,91 +1,97 @@
-from src.sgd.convert import basic_convert, remove_nones
-from src.sgd.convert.util import get_relation_to_ro_id
-from sqlalchemy.orm import joinedload
+from src.sgd.convert import basic_convert
+from src.sgd.convert.util import children_from_obo, get_relation_to_ro_id
 
-__author__ = 'kpaskov'
-## updated by sweng66
+__author__ = 'sweng66'
 
-def load_aliases(bud_obj, bud_session, secondary_common_name):
-    from src.sgd.model.bud.taxonomy import TaxonomyAlias
-
-    aliases = []
-    for alias_obj in bud_session.query(TaxonomyAlias).filter_by(taxon_id=bud_obj.id).all():
-        aliases.append(remove_nones({
-            "display_name": alias_obj.synonym,
-            "alias_type": 'Synonym',
-            "source": {'display_name': 'NCBI'},
-            "bud_id": int(alias_obj.id)
-        }))
-
-    for common_name in secondary_common_name:
-        aliases.append({
-            "display_name": common_name,
-            "alias_type": 'Secondary common name',
-            "source": {'display_name': 'NCBI'}
-        })
-
-    return aliases
-
-
-def load_relations(bud_obj, bud_session):
-    from src.sgd.model.bud.taxonomy import TaxonomyRelation
-
-    relations = []
-    for relation_obj in bud_session.query(TaxonomyRelation).filter_by(parent_id=bud_obj.id).filter_by(generation=1).all():
-        relations.append(remove_nones({
-            "display_name": relation_obj.child.name,
-            "taxid": relation_obj.child.id,
-            "rank": relation_obj.child.rank,
-            "date_created": str(relation_obj.child.date_created),
-            "created_by": relation_obj.child.created_by,
-            'source': {'display_name': 'NCBI'},
-            "ro_id": get_relation_to_ro_id('is a')
-        }))
-    return relations
-
+key_switch = {'name': 'display_name', 'def': 'description'}
+ancestor = 'NCBITaxon:4893'
+source = 'NCBI'
 
 def taxonomy_starter(bud_session_maker):
-    from src.sgd.model.bud.taxonomy import TaxonomyRelation
-    bud_session = bud_session_maker()
+    
+    ## downloaded from http://www.berkeleybop.org/ontologies/ncbitaxon.obo
+    f = open('src/sgd/convert/data/ncbitaxon.obo', 'r')
+    
+    print "getting children of taxon_id=4893"
 
-    # only load taxonomy for family "Saccharomycetaceae" (taxon_id=4893) and lower
-    for relation in bud_session.query(TaxonomyRelation).options(joinedload(TaxonomyRelation.child)).filter_by(parent_id=4893).all():
-        bud_obj = relation.child
+    [filtered_set, id_to_rank] = children_from_obo('src/sgd/convert/data/ncbitaxon.obo',
+                                                   ancestor)
 
-        obj_json = {'display_name': bud_obj.name,                  
-                    'taxid': int(bud_obj.id),
-                    'rank': bud_obj.rank,    
-                    'source': {'display_name': 'NCBI'},
-                    'date_created': str(bud_obj.date_created),                  
-                    'created_by': bud_obj.created_by}     
+    ## total 1018 in the filtered set
+    ## print "COUNT=", len(filtered_set)
 
-        common_name_str = bud_obj.common_name
-        
-        secondary_common_name = []
-        if common_name_str != None:
-            common_name_list = common_name_str.split("|") 
-            name = common_name_list.pop(0)
-            obj_json['common_name'] = name
-            secondary_common_name = common_name_list
+    print "reading info for taxon_id 4893 and its children"
 
-        #Load aliases
-        obj_json['aliases'] = load_aliases(bud_obj, bud_session, secondary_common_name)
-        
-        #Load children
-        obj_json['children'] = load_relations(bud_obj, bud_session)
+    terms = []
+    parent_to_children = dict()
+    term = None
+    for line in f:
+        line = line.strip()
+        pieces = line.split(': ')
+        if line == '[Term]':
+            continue
+        elif pieces[0] == 'id':
+            if term is not None:
+                terms.append(term)
+                term = None
+            if pieces[1] in filtered_set:
+                taxid = pieces[1].replace("NCBITaxon:", "")
+                term = {'taxid': int(taxid),
+                        'aliases': [],
+                        'source': {'display_name': source},
+                        'urls': []}
+        elif term is not None:
+            pieces = line.split(': ')
+            if len(pieces) == 2:
+                if pieces[0] == 'synonym':
+                    quotation_split = pieces[1].split('"')
+                    if 'EXACT common_name' in quotation_split[2]:
+                        term['common_name'] = quotation_split[1]
+                    else:
+                        alias_type = 'Synonym'
+                        if 'common_name' in quotation_split[2]:
+                            alias_type = 'Secondary common name'
+                        term['aliases'].append({'display_name': quotation_split[1], "alias_type": alias_type, "source": {"display_name": source}})
+                elif pieces[0] == 'is_a':
+                    parent = pieces[1].split('!')[0].strip()
+                    rank = id_to_rank.get(parent)
+                    if rank is None:
+                        rank = 'no rank'
+                    parent = parent.replace("NCBITaxon:", "")
+                    parent = int(parent)
+                    if parent not in parent_to_children:
+                        parent_to_children[parent] = []
+                    parent_to_children[parent].append({'taxid': term['taxid'], 'display_name': term['display_name'], 'source': {'display_name': source}, 'rank': rank, 'ro_id': get_relation_to_ro_id('is a')})
+                elif pieces[0] in key_switch:
+                    text = pieces[1]
+                    quotation_split = pieces[1].split('"')
+                    if pieces[0] == 'def':
+                        text = quotation_split[1]
+                    term[key_switch[pieces[0]]] = text
+                elif pieces[0] == 'property_value' and pieces[1].startswith('has_rank NCBITaxon:'):
+                    term['rank'] = pieces[1].replace("has_rank NCBITaxon:", "")
+    f.close()
 
-        obj_json['urls'] = [{"display_name": 'NCBI Taxonomy',
-                             "url_type": 'NCBI Taxonomy',
-                             "source": {'display_name': 'NCBI'},
-                             "link": "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+str(bud_obj.id)}]
+    print "updating the database"
 
-        yield obj_json
+    for term in terms:
+        taxid = term['taxid']
+        print taxid
+        if term.get('rank') is None:
+            term['rank'] = 'no rank'
+        term['children'] = [] if taxid not in parent_to_children else parent_to_children[taxid]
 
-    bud_session.close()
+        term['urls'] = [{"display_name": 'NCBI Taxonomy',
+                         "url_type": 'NCBI Taxonomy',
+                         "source": {'display_name': source},
+                         "link": "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+str(taxid)}]
+        yield term
+
 
 if __name__ == '__main__':
     from src.sgd.convert import config
     basic_convert(config.BUD_HOST, config.NEX_HOST, taxonomy_starter, 'taxonomy', lambda x: x['display_name'])
+
 
 
 
