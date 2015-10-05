47c8173beb56c0294dffeb339b54dfc8de35538e
Travis Sheppard <tshepp@stanford.edu>
2015-10-05 15:02:33 -0700

    POC db query

---
+++ src/sgd/convert/query_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import *
+from src.sgd.convert import prepare_schema_connection
+from src.sgd.convert import config
+from src.sgd.model import nex
+
+def main():
+    nex_session_maker = prepare_schema_connection(nex, config.NEX_DBTYPE, config.NEX_HOST, config.NEX_DBNAME, config.NEX_SCHEMA, config.NEX_DBUSER, config.NEX_DBPASS)
+    from src.sgd.model.nex.bioentity import Bioentity
+    nex_session = nex_session_maker()
+    for bioent in nex_session.query(Bioentity).all():
+        print bioent.id
+
+main()
