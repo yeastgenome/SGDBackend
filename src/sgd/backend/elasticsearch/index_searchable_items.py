c4cd1f2a733c311a29f394726ffa574851ddcefb
Travis Sheppard <tshepp@stanford.edu>
2015-10-08 09:53:34 -0700

    index a few more data types

--- src/sgd/backend/elasticsearch/index_searchable_items.py
+++ src/sgd/backend/elasticsearch/index_searchable_items.py
@@ -17,6 +17,8 @@ perf_session_maker = prepare_schema_connection(perf, config.PERF_DBTYPE, config.
 from src.sgd.model.nex.bioentity import Bioentity
 from src.sgd.model.perf.core import Bioentity as PerfBioentity
 from src.sgd.model.nex.reference import Author
+from src.sgd.model.nex.misc import Strain
+from src.sgd.model.nex.bioconcept import Go
 nex_session = nex_session_maker()
 perf_session = perf_session_maker()
 
@@ -34,6 +36,7 @@ def put_mapping():
     return
 
 def index_genes():
+    print 'indexing genes'
     # get nex format of all genes
     all_genes = nex_session.query(Bioentity).all()
     for gene in all_genes:
@@ -50,12 +53,13 @@ def index_genes():
             'category': 'locus',
             'data': perf_result.to_json()
         }
-        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj)
+        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj, id=gene.sgdid)
 
 def index_phenotypes():
     return
 
 def index_authors():
+    print 'indexing authors'
     # get nex format of all authors
     all_authors = nex_session.query(Author).all()
     for author in all_authors:
@@ -66,23 +70,43 @@ def index_authors():
             'category': 'author',
             'data': {}
         }
-        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj)
+        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj, id=author.id)
 
+def index_strains():
+    print 'indexing strains'
+    all_strains = nex_session.query(Strain).all()
+    for strain in all_strains:
+        obj = {
+            'name': strain.display_name,
+            'href': strain.link,
+            'description': strain.description,
+            'category': 'strain',
+            'data': {}
+        }
+        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj, id=strain.id)
+
+def index_go_terms():
+    print 'indexing GO terms'
+    all_gos = nex_session.query(Go).all()
+    for go in all_gos:
+        obj = {
+            'name': go.display_name,
+            'href': go.link,
+            'description': go.description,
+            'category': 'function',
+            'data': {}
+        }
+        es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=obj, id=go.sgdid)
 
 def main():
     setup_index()
-    # index_genes()
-    # index_phenotypes()
+    index_genes()
+    index_phenotypes()
     index_authors()
-
-    # functions
+    index_strains()
+    index_go_terms()
 
     # experiments
-
-    # authors
-
-    # strains
-
     # pages
 
 main()
