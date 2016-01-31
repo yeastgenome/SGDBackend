c5172513330986f27b5bd2ad08cea8dc9448e8ff
Travis <travissheppard.1988@gmail.com>
2016-01-30 17:26:17 -0800

    update layout

--- client/jsx/containers/dashboard.js
+++ client/jsx/containers/dashboard.js
@@ -4,14 +4,18 @@ import { Link } from 'react-router';
 const Dashboard = React.createClass({
   render() {
     return (
-      <div>
-        <div className='col-sm-1'>
-          <div className='btn-group-vertical' role='group'>
-            <Link to='/dashboard' className='btn btn-default'><span className='glyphicon glyphicon-home' /> Home</Link>
-            <Link to='/dashboard/files' className='btn btn-default'><span className='glyphicon glyphicon-floppy-disk' /> Files</Link>
-          </div>
+      <div className='row'>
+        <div className='columns small-2'>
+          <ul className='vertical menu'>
+            <li>
+              <Link to='/dashboard' className='btn btn-default'><i className='fa fa-home' /> Home</Link>
+            </li>
+            <li>
+              <Link to='/dashboard/files' className='btn btn-default'><i className='fa fa-floppy-o' /> Files</Link>
+            </li>
+          </ul>
         </div>
-        <div className='col-sm-11'>
+        <div className='columns small-10'>
           {this.props.children}
         </div>
       </div>
--- client/jsx/containers/layout.js
+++ client/jsx/containers/layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Radium from 'radium';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import * as AuthActions from '../actions/auth_actions';
@@ -9,39 +10,62 @@ const AppLayout = React.createClass({
       e.preventDefault();
       this.props.dispatch(AuthActions.logoutAndRedirect());
     };
+    // init auth nodes, either login or logout links
     let authNodes = this.props.isAuthenticated ?
-      <ul className='nav navbar-nav navbar-right'><li><Link to='/account'><span className='glyphicon glyphicon-user'></span> {this.props.username}</Link></li><li><a onClick={onClickLogout} href='#'><span className='glyphicon glyphicon-log-out'></span> Logout</a></li></ul> :
-      <ul className='nav navbar-nav navbar-right'><li><Link to='/login'><span className='glyphicon glyphicon-log-in'></span> Login</Link></li></ul>
+      <ul style={styles.menu} className='menu'><li><Link style={styles.navLink} to='/account'><i className='fa fa-user'></i> {this.props.username}</Link></li><li><a style={styles.navLink} onClick={onClickLogout} href='#'><i className='fa fa-sign-out'></i> Logout</a></li></ul> :
+      <ul style={styles.menu} className='menu'><li><Link style={styles.navLink} to='/login'><i className='fa fa-sign-in'></i> Login</Link></li></ul>;
     return (
       <div>
-        <nav className='navbar navbar-inverse navbar-fixed-top'>
-          <div className='container-fluid'>
-            <div className='navbar-header'>
-              <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
-                <span className='sr-only'>Toggle navigation</span>
-                <span className='icon-bar'></span>
-                <span className='icon-bar'></span>
-                <span className='icon-bar'></span>
-              </button>
-              <Link className='navbar-brand' to='/dashboard'>SGD Admin</Link>
-            </div>
-            <div id='navbar' className='navbar-collapse collapse'>
-              {authNodes}
-            </div>
-          </div>
+        <nav className='top-bar' style={styles.messageZone}>
+          <p>Have a look at our <a>cookies policy</a>. and <a>privacy policy.</a></p>
         </nav>
-
-        <div className='container-fluid'>
-          <div className='row application-row' style={{ marginTop: '7rem' }}>
-            <div className='col-sm-12'>
-              {this.props.children}
-            </div>
+        <nav className='top-bar' style={styles.navWrapper}>
+          <div className='top-bar-left'>
+            <ul className='menu' style={styles.menu}>
+              <li>
+                <Link to='dashboard' style={styles.navLink}>
+                  <p>SGD Admin</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+         <div className='top-bar-right'>
+          {authNodes}
+        </div>
+        </nav>
+        <div className='row full-width wrapper'>
+          <div className='large-12 columns'>
+            {this.props.children}
           </div>
         </div>
       </div>
     );
   }
 });
+
+const purple = '#663882';
+const red = '#C22D38';
+const messageColor = '#CCC';
+var styles = {
+  messageZone: {
+    background: 'black',
+    color: messageColor
+  },
+  menu: {
+    background: 'none',
+    fontSize: '18px'
+  },
+  navWrapper: {
+    background: purple
+  },
+  navLink: {
+    color: 'white',
+    background: 'none'
+  },
+  indexLink: {
+    color: 'black'
+  }
+};
 
 function mapStateToProps(_state) {
   let state = _state.auth;
@@ -52,4 +76,4 @@ function mapStateToProps(_state) {
   };
 };
 
-module.exports = connect(mapStateToProps)(AppLayout);
+module.exports = Radium(connect(mapStateToProps)(AppLayout));
