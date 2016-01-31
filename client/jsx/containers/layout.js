931504c65dc9e3f9e638f072497d2bf2223035cd
Travis <travissheppard.1988@gmail.com>
2016-01-30 17:49:49 -0800

    more style

--- client/jsx/containers/layout.js
+++ client/jsx/containers/layout.js
@@ -12,19 +12,19 @@ const AppLayout = React.createClass({
     };
     // init auth nodes, either login or logout links
     let authNodes = this.props.isAuthenticated ?
-      <ul style={styles.menu} className='menu'><li><Link style={styles.navLink} to='/account'><i className='fa fa-user'></i> {this.props.username}</Link></li><li><a style={styles.navLink} onClick={onClickLogout} href='#'><i className='fa fa-sign-out'></i> Logout</a></li></ul> :
-      <ul style={styles.menu} className='menu'><li><Link style={styles.navLink} to='/login'><i className='fa fa-sign-in'></i> Login</Link></li></ul>;
+      <ul style={styles.authMenu} className='menu'><li><Link style={styles.navLink} to='/account'><i className='fa fa-user'></i> {this.props.username}</Link></li><li><a style={styles.navLink} onClick={onClickLogout} href='#'><i className='fa fa-sign-out'></i> Logout</a></li></ul> :
+      <ul style={styles.authMenu} className='menu'><li><Link style={styles.navLink} to='/login'><i className='fa fa-sign-in'></i> Login</Link></li></ul>;
     return (
       <div>
         <nav className='top-bar' style={styles.messageZone}>
-          <p>Have a look at our <a>cookies policy</a>. and <a>privacy policy.</a></p>
+          <p style={styles.messageText}>Have a look at our <a>cookies policy</a> and <a>privacy policy.</a></p>
         </nav>
         <nav className='top-bar' style={styles.navWrapper}>
           <div className='top-bar-left'>
             <ul className='menu' style={styles.menu}>
               <li>
-                <Link to='dashboard' style={styles.navLink}>
-                  <p>SGD Admin</p>
+                <Link to='dashboard' style={styles.indexLink}>
+                  <img src='img/sgd-logo.png' /><span style={styles.indexText}>Admin</span>
                 </Link>
               </li>
             </ul>
@@ -51,19 +51,30 @@ var styles = {
     background: 'black',
     color: messageColor
   },
+  messageText: {
+    marginBottom: '0.25rem'
+  },
   menu: {
     background: 'none',
     fontSize: '18px'
+  },
+  authMenu: {
+    background: 'none',
+    fontSize: '18px',
+    marginTop: '0.25rem'
   },
   navWrapper: {
     background: purple
   },
   navLink: {
     color: 'white',
-    background: 'none'
   },
   indexLink: {
-    color: 'black'
+    color: 'white',
+    padding: '0.25rem'
+  },
+  indexText: {
+    fontSize: 28
   }
 };
 
